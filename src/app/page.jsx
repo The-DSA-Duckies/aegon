ee054b2cebfc60e10ae9f29b5923e2fdacba9ac8
Kevin Allen <64329665+KAllen2024@users.noreply.github.com>
2024-03-23 19:03:03 -0400

    First iteration of landing page complete. Does not contain "Getting Started" button. Added LandingDucks.png to images.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,27 +2,26 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Image from 'next/image';
-import Head from 'next/head';
 import { TopBarLayout } from "./layout";
 
 export default function Home() {
   return (
     <TopBarLayout>
       <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh - 64px' }}>
-        <Box sx={{ width: '50%', padding: '0 20px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-          <Typography variant="h1" component="h1" sx={{ }}>
-            Grading Made 
+        <Box sx={{ width: '50%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+          <Typography variant="h1" sx={{paddingTop: '1.5em', fontWeight: 'bold' }}>
+            Grading Made<br/>
             <span style={{ color: '#1c65ee' }}> Simple</span>
           </Typography>
-          <Typography variant="h4" sx={{ }}>
-            Guppie Grader optimizes your
+          <Typography variant="h4" sx={{paddingTop: '1em', fontWeight: 'bold' }}>
+            Guppie Grader optimizes<br/> your
             <span style={{ color: '#fbac13' }}> grading workflow </span>
-            by processing code and reports using advanced
-            <span style={{ color: '#fbac13' }}> LLM technology</span>.
+            by<br/> processing code and reports<br/> using advanced
+            <span style={{ color: '#fbac13' }}> LLM<br/> technology</span>.
           </Typography>
         </Box>
-        <Box sx={{ width: '50%' }}>
-          <Image src="/images/logo.png" alt="Your Image" width={500} height={500}/>
+        <Box sx={{ width: '50%', display: 'flex', justifyContent: 'center', alignItems: 'center', paddingTop: '5em' }}>
+          <Image src="/images/LandingDucks.png" alt="Landing page image" width={500} height={200}/>
         </Box>
       </Box>
     </TopBarLayout>
