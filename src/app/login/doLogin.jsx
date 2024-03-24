0b0b1c207a6b6f97f54b8ebe465733c6e98b9dd1
Kevin Allen <64329665+KAllen2024@users.noreply.github.com>
2024-03-24 18:31:41 -0400

    Working login page functionality using hard-coded username and password values for demo.

--- src/app/login/doLogin.jsx
+++ src/app/login/doLogin.jsx
@@ -3,28 +3,43 @@ import React, { useState } from 'react';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
+import { useRouter } from 'next/navigation';
 
 export default function DoLogin() {
+  const router = useRouter();
+
+  const handleNavigate = (path) => {
+    router.push(path);
+  };
+
     // State to manage the value of the input field
-    const [userValue, setUserValue] = useState('');
-    const [passValue, setPassValue] = useState('');
+  const [userValue, setUserValue] = useState('');
+  const [passValue, setPassValue] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
     // Function to handle changes in the input field
-    const handleUserChange = (event) => {
-      setUserValue(event.target.value);
-    };
+  const handleUserChange = (event) => {
+    setUserValue(event.target.value);
+  };
 
-    const handlePassChange = (event) => {
-      setPassValue(event.target.value);
-    };
+  const handlePassChange = (event) => {
+    setPassValue(event.target.value);
+  };
 
     // Function to handle form submission
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        // Do something with the input value, such as sending it to an API or processing it
-        console.log('User value: ', userValue);
-        console.log('Pass value: ', passValue);
-    };
+  const handleSubmit = (event) => {
+      event.preventDefault();
+      console.log('User value: ', userValue);
+      console.log('Pass value: ', passValue);
+        
+        // temporary hard-coded login account that doesn't actually restrict anything
+      if (userValue == "test" && passValue == "12345678") {
+          handleNavigate('/assignments');
+      }
+      else {
+          setErrorMessage('Incorrect username or password. Please try again.');
+      }
+  };
 
   return (
     <form onSubmit={handleSubmit}>
@@ -39,6 +54,11 @@ export default function DoLogin() {
       <Button type="submit" sx={{fontSize: '2rem', padding: '0.5em 1em', marginTop: '1em', color: 'white', backgroundColor: '#1c65ee', whiteSpace: 'nowrap', fontWeight: 'bold', '&:hover': { backgroundColor: '#1c65ee'} }}>
         Login
       </Button>
+      {errorMessage && (
+        <Typography variant="body1" color="red" sx={{ paddingTop: '1em' }}>
+          {errorMessage}
+        </Typography>
+      )}
     </form>
   );
 }
